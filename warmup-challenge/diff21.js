4a5167fb37e0693a57a43c38971bb7be0a22c0f7
BilalHassen <bilalhassen097@gmail.com>
2024-03-19 17:39:22 -0400

    solution for parrotTrouble

--- warmup-challenge/diff21.js
+++ warmup-challenge/diff21.js
@@ -4,6 +4,7 @@ Given an int n, return the absolute difference between n and 21,
  except return double the absolute difference if n is over 21.
 */
 
+// opted not to use Math.abs
 function diff21(n) {
   const absDifference = 21 - n;
   if (n <= 21) {
