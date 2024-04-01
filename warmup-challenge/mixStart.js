f2874f8585cf5d6ddac83d4ea01779091f6b3c99
BilalHassen <bilalhassen097@gmail.com>
2024-04-01 07:28:00 -0400

    solution for mixStart

--- warmup-challenge/mixStart.js
+++ warmup-challenge/mixStart.js
@@ -3,3 +3,7 @@ Warmup-1 -- mixStart
 Return true if the given string begins with 'mix', 
 except the 'm' can be anything, so 'pix', '9ix' .. all count.
 */
+
+function mixStart(str) {
+  return str.charAt(1) === "i" && str.charAt(2) === "x";
+}
