6641978e52771cf0da25750286d539123d93c400
Bilal Hassen <bilalhassen097@gmail.com>
2024-04-21 18:19:56 -0400

    remove logs

--- warmup-challenge/doubleX.js
+++ warmup-challenge/doubleX.js
@@ -2,26 +2,19 @@
 Given a string, return true if the first instance of "x" 
 in the string is immediately followed by another "x". */
 
-function doubleX(str){
-    if(str === ""){
-        return false
-    }
-  for(let i = 0; i < str.length; i++){
-    if(str[i] === "x"  && str[i + 1] === "x"){
-        console.log("from first if")
-        return true;
-    }  else if(str[i] !== "x" && str[i + 1] !== "x"){
-        console.log("from second line")
-        return false;
-    } else if(str[i] !== "x" && str[i + 1] === "x" && str[i + 2] === "x"){
-        return true;
-    }else{
-        console.log("from last line")
-        return false;
-    }
- 
+function doubleX(str) {
+  if (str === "") {
+    return false;
   }
-
-
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === "x" && str[i + 1] === "x") {
+      return true;
+    } else if (str[i] !== "x" && str[i + 1] !== "x") {
+      return false;
+    } else if (str[i] !== "x" && str[i + 1] === "x" && str[i + 2] === "x") {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
-
