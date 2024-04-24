2b9bfda4984c4243c86453d14d2b8c9e7d992b98
Bilal Hassen <bilalhassen097@gmail.com>
2024-04-24 16:51:12 -0400

    remove log

--- warmup-challenge/stringMatch.js
+++ warmup-challenge/stringMatch.js
@@ -10,7 +10,6 @@ substrings appear in the same place in both strings.
 function stringMatch(a, b){
     let count = 0;
     for(let i = 0; i < a.length -1 && b.length -1; i++){
-        console.log(a[i], b[i])
         if(a[i] === b[i] && a[i + 1] === b[i + 1]){
             count += 1
             console.log(count)
