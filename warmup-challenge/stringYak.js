91d0f815c27e2bd6eb2b7be78acb6e444e0e3b28
BilalHassen <bilalhassen097@gmail.com>
2024-04-30 14:54:49 -0400

    changes to commit history

---
+++ warmup-challenge/stringYak.js
@@ -0,0 +1,14 @@
+// Warmup-2 -- stringYak
+// Suppose the string "yak" is unlucky.
+//Given a string, return a version where all the "yak" are removed,
+//but the "a" can be any char. The "yak" strings will not overlap.
+
+// Examples
+
+// stringYak('yakpak') → pak
+// stringYak('pakyak') → pak
+// stringYak('yak123ya') → 123ya
+
+function stringYak(str) {}
+
+console.log(stringYak("yakpak"));
