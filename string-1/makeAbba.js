6aac656e60a8abecb94564efe060d7c98ce6896d
BilalHassen <bilalhassen097@gmail.com>
2024-05-06 11:05:29 -0400

    solution for makeAbba

---
+++ string-1/makeAbba.js
@@ -0,0 +1,12 @@
+/*
+String-1 -- makeAbba
+Given two strings, a and b, 
+return the result of putting them together in the order abba, 
+e.g. "Hi" and "Bye" returns "HiByeByeHi".
+*/
+
+function makeAbba(a, b) {
+  return `${a}${b}${b}${a}`;
+}
+
+console.log(makeAbba("Hi", "Bye"));
