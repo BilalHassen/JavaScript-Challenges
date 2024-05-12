87295eef101db5870cbb0687ff94ced48c5b5127
Bilal Hassen <bilalhassen097@gmail.com>
2024-05-12 12:03:47 -0400

    solution for wihtoutEnd

---
+++ string-1/withoutEnd.js
@@ -0,0 +1,12 @@
+/*
+String-1 -- withoutEnd
+Given a string, 
+return a version without the first and last char, 
+so "Hello" yields "ell". The string length will be at least 2.
+*/ 
+
+function withoutEnd(str){
+  return str.slice(1, str.length-1)
+}
+
+console.log(withoutEnd("Hello"))
