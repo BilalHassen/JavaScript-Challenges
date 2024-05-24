326944fcbedf17502ed1837a461853a5dc68b597
BilalHassen <bilalhassen097@gmail.com>
2024-05-24 10:39:40 -0400

    solution for lastTwo

---
+++ string-1/lastTwo.js
@@ -0,0 +1,16 @@
+/*
+String-1 -- lastTwo
+Given a string of any length, 
+return a new string where the last 2 chars, 
+if present, are swapped, so "coding" yields "codign".
+*/
+
+function lastTwo(str) {
+  let swappedOne = str.substring(str.length - 2);
+  if (str.length >= 2) {
+    return str.slice(0, str.length - 2) + (swappedOne[1] + swappedOne[0]);
+  }
+  return str;
+}
+
+console.log(lastTwo(""));
