4ab9ba8fa3a58adfb457fb01960acab54b1a00d5
BilalHassen <bilalhassen097@gmail.com>
2024-06-27 10:54:41 -0400

    add shorter version of sortaSum

--- Logic-1/sortaSum.js
+++ Logic-1/sortaSum.js
@@ -3,12 +3,17 @@ Logic-1 -- sortaSum
 Given 2 ints, a and b, return their sum. However, sums in the range 10..19 inclusive, are forbidden, so in that case just return 20.
 */
 
-function sortaSum(a, b) {
-  if (a + b >= 10 && a + b <= 19) {
-    return 20;
-  } else {
-    return a + b;
-  }
-}
+// function sortaSum(a, b) {
+//   if (a + b >= 10 && a + b <= 19) {
+//     return 20;
+//   } else {
+//     return a + b;
+//   }
+// }
 
-console.log(sortaSum(10, 11));
+// console.log(sortaSum(10, 11));
+
+// shoter version
+function sortaSum(a, b) {
+  return a + b >= 10 && a + b <= 19 ? 20 : a + b;
+}
