d149b6e1f4c22c3cfc243b07191391816e551d4a
BilalHassen <bilalhassen097@gmail.com>
2024-09-08 13:33:12 -0400

    solution for LastDigit

---
+++ Logic-1/LastDigit.js
@@ -0,0 +1,11 @@
+/*
+Warmup-1 -- lastDigit
+Given two non-negative int values, 
+return true if they have the same last digit, 
+such as with 27 and 57. 
+Note that the % 'mod' operator computes remainders, so 17 % 10 is 7.
+*/
+
+function lastDigit(a, b, c) {
+  return a % 10 === b || b % 10 === a;
+}
