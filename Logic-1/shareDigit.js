662cabf5cd5efaee0737ffecc11e07d8ec1f7cfb
BilalHassen <bilalhassen097@gmail.com>
2024-10-06 12:54:33 -0400

    solution for shareDigit

---
+++ Logic-1/shareDigit.js
@@ -0,0 +1,22 @@
+/*
+Logic-1 -- shareDigit
+Given two ints, each in the range 10..99, 
+return true if there is a digit that appears in both numbers, 
+such as the 2 in 12 and 23. 
+(Note: division, e.g. n/10, gives the left digit while 
+the % "mod" n%10 gives the right digit.)
+*/
+
+function shareDigit(a, b) {
+  if (
+    Math.floor(a / 10) === Math.floor(b / 10) ||
+    a % 10 === b % 10 ||
+    a % 10 === Math.floor(b / 10) ||
+    b % 10 === Math.floor(a / 10)
+  ) {
+    return true;
+  }
+  return false;
+}
+
+console.log(shareDigit(12, 23));
